fd846e9d57158b23e54ed7d175bdf105ab2dfebd
William Alvis <alvis.wap@gmail.com>
2020-11-14 17:10:35 -0500

    Account page

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
               theme: lightTheme,
               initialRoute: '/',
               routes: {
-                '/': (BuildContext context) => AccountPage(),
+                '/': (BuildContext context) => HomePage(),
                 '/phone_number': (BuildContext context) => PhoneNumberPage(),
                 '/select_location': (BuildContext context) => SelectAddressPage(),
                 '/sing_in': (BuildContext context) => SingInPage(),
--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -9,6 +9,34 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomCustomAppBar(
+          children: [
+            IconStatusCustomBar(
+              icon: icon,
+              textIcon: textIcon,
+              onPressed: this.onPressed,
+            ),
+            IconStatusCustomBar(icon: icon, textIcon: textIcon, onPressed: this.onPressed),
+            IconStatusCustomBar(icon: icon, textIcon: textIcon, onPressed: this.onPressed),
+            IconStatusCustomBar(icon: icon, textIcon: textIcon, onPressed: this.onPressed),
+
+            Expanded(
+              child: Container(
+                width: double.infinity,
+              ),
+            ), // The dummy child
+
+            IconStatusCustomBar(
+              icon: icon,
+              textIcon: textIcon,
+              onPressed: this.onPressed,
+            ),
+          ],
+          textIcon: 'Home',
+          icon: Icon(Icons.home_outlined),
+          onPressed: () {
+            debugPrint('Icono presionado');
+          }),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(left: 20.0, right: 20, top: 50),
@@ -74,6 +102,74 @@ class HomePage extends StatelessWidget {
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomCustomAppBar extends StatelessWidget {
+  final Icon icon;
+  final String textIcon;
+  final Function onPressed;
+  final List<Widget> children;
+
+  const BottomCustomAppBar({
+    Key key,
+    @required this.icon,
+    this.textIcon,
+    this.onPressed,
+    this.children,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      child: ClipRRect(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+        child: BottomAppBar(
+          notchMargin: 4,
+          color: Colors.white,
+          shape: CircularNotchedRectangle(),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: children,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class IconStatusCustomBar extends StatelessWidget {
+  const IconStatusCustomBar({
+    Key key,
+    @required this.icon,
+    @required this.textIcon,
+    this.onPressed,
+  }) : super(key: key);
+
+  final Icon icon;
+  final String textIcon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon,
+            Text(textIcon),
+          ],
         ),
       ),
     );
