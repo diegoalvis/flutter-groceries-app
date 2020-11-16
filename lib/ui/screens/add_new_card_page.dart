2e5f300d7f7e858860e008b18423ae5a403f3514
William Alvis <alvis.wap@gmail.com>
2020-11-16 17:07:14 -0500

    init card pay

---
+++ lib/ui/screens/add_new_card_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:awesome_card/awesome_card.dart';
+
+class AddNewCardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
