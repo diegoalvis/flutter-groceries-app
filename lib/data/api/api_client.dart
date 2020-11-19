ea780c4735c0b8170e4e3b1bbb49fb19f401c4f5
Diego Alvis <diegoalvispal@gmail.com>
2020-11-19 01:05:14 +0100

    Change base url to stg

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -9,10 +9,9 @@ import 'package:winkels_customer/data/models/VendorCategory.dart';
 import 'package:winkels_customer/data/models/VendorProduct.dart';
 
 class ApiClient {
-  static const String BASE_URL = "http://10.0.2.2:1337";
-
+  // static const String BASE_URL = "http://10.0.2.2:1337";
   // Testing Heroku
-  // static const String BASE_URL = "https://winkels-strapi.herokuapp.com";
+  static const String BASE_URL = "https://winkels-strapi.herokuapp.com";
   // Prod
   // static const String BASE_URL = "https://winkels-strapi.herokuapp.com";
   final Dio _dio;
